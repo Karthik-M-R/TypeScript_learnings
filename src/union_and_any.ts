b7faecfea42ebef2220386824769a46353dfa7a1
Karthik <karthikmr135@gmail.com>
2026-02-18 23:27:11 +0530

    learnt about annotation and inference and union,any

--- src/union_and_any.ts
+++ src/union_and_any.ts
@@ -5,3 +5,17 @@ let subs:number | string='1M'; //union, here subs can be num or string
 let apiReqStatus:'pending'|'sucess' |'error'='pending'  ;
 apiReqStatus='sucess';
 // apiReqStatus='done'; cant use only 'pending'|'sucess' |'error'
+const orders=['21','22','23'];
+let currentorder: string | undefined;
+
+for(let o of orders)
+{
+    if(o=='23')
+    {
+        currentorder=o;
+        break;
+    }
+  currentorder='13';
+
+}
+console.log(currentorder);
